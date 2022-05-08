ace16f9ad71b93159e3230b928b2a9b5dd3bf6a5
SuhaasNandeesh <suhaasnandeesh@gmail.com>
2022-05-08 12:54:55 +0530

    revert

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 mongoose
+<<<<<<< HEAD
     .connect('mongodb://localhost:27017/cinema', { useNewUrlParser: true })
+=======
+    .connect('mongodb://23.22.111.212:5000/cinema', { useNewUrlParser: true })
+>>>>>>> parent of 0077b1a (Revert "mongodb default port update")
     .catch(e => {
         console.error('Connection error', e.message)
     })
